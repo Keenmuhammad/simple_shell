bd4f87279ee3596ec815db40c43d49a45dfe182c
Keenmuhammad <kibrahimbello@yahoo.com>
2022-11-10 02:57:45 -0800

    Day 1

--- _atoi.c
+++ _atoi.c
@@ -1,10 +1,10 @@
 #include "shell.h"
 
 /**
- * interactive - returns true if shell is interactive mode
+ * interactive - returns true if shell is interactive
  * @info: struct address
  *
- * Return: 1 if interactive mode, 0 otherwise
+ * Return: 1 if shell is  interactive, 0 otherwise
  */
 int interactive(info_t *info)
 {
